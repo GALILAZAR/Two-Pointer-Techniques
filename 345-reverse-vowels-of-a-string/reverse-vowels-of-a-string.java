42c7356d8816c318de3d46254d056f285859f522
GALI LAZAR <thalapathyasuran@gmail.com>
2026-06-05 12:33:25 +0530

    Time: 2 ms (99.34%) | Memory: 46.5 MB (65.76%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
@@ -3,31 +3,26 @@ class Solution {
         char[] arr = s.toCharArray();
         int left = 0;
         int right = arr.length - 1;
-        boolean flag1 = false, flag2 = false;
         while (left < right) {
-            char ch1 = Character.toLowerCase(arr[left]);
-            char ch2 = Character.toLowerCase(arr[right]);
-            if(!flag1) {
+            while(left < right) {
+                char ch1 = Character.toLowerCase(arr[left]);
                 if (ch1 == 'a' || ch1 == 'e' || ch1 == 'i' || ch1 == 'o' || ch1 == 'u') {
-                    flag1 = true;
+                    break;
                 } else {
                     left++;
                 }
             }
-            if (!flag2) {
+            while(right > left) {
+                char ch2 = Character.toLowerCase(arr[right]);
                 if (ch2 == 'a' || ch2 == 'e' || ch2 == 'i' || ch2 == 'o' || ch2 == 'u') {
-                    flag2 = true;
+                    break;
                 } else {
                     right--;
                 }
             }
-            if (flag1 && flag2) {
-                char temp = arr[left];
-                arr[left++] = arr[right];
-                arr[right--] = temp;
-                flag1 = false;
-                flag2 = false;
-            }
+            char temp = arr[left];
+            arr[left++] = arr[right];
+            arr[right--] = temp;
         }
         return new String(arr);
     }
