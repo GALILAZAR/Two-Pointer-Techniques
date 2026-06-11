5bbbf0d065efe0576536158488e1c0a990314f7a
GALI LAZAR <thalapathyasuran@gmail.com>
2026-06-11 12:18:13 +0530

    Time: 2 ms (10.43%) | Memory: 43 MB (68.06%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -3,11 +3,6 @@ class Solution {
 
         String [] arr = s.trim().split("\\s+");
 
-        for(int i = arr.length-1; i>=0; i--)
-        {
-            break;
-        }
-
         return arr[arr.length-1].length();
     }
 }
