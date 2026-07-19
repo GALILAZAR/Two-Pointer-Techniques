d8a44f867072ca320a836dc3f964c5330146440e
GALI LAZAR <thalapathyasuran@gmail.com>
2026-07-19 16:33:13 +0530

    Time: 1 ms (52.62%) | Memory: 57.8 MB (6.51%) - LeetSync

---
+++ 258-add-digits/add-digits.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var addDigits = function(num) {
+
+    
+
+    while(num >=10)
+    {
+        let sum = 0;
+        while(num!=0)
+        {
+            let digit = num % 10;
+            sum = sum+digit;
+            num = Math.floor(num / 10);
+
+        }
+        num = sum;
+    }
+    return num;
+    
+};
