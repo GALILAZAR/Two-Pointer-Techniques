2a85ed6034ec009e6bf9a26f05b040863ea8544d
GALI LAZAR <thalapathyasuran@gmail.com>
2026-08-07 19:10:16 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (63.89%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -5,7 +5,7 @@ class Solution {
 
         int i = 0;  // pointer for nums1
         int j = 0;  // pointer for nums2
-        int k = 0;  // pointer for result
+        int k = 0; 
 
         // Merge both arrays
         while (i < m && j < n) {
@@ -19,21 +19,18 @@ class Solution {
             k++;
         }
 
-        // Copy remaining elements of nums1
         while (i < m) {
             result[k] = nums1[i];
             i++;
             k++;
         }
 
-        // Copy remaining elements of nums2
         while (j < n) {
             result[k] = nums2[j];
             j++;
             k++;
         }
 
-        // Copy result back to nums1
         for (int x = 0; x < m + n; x++) {
             nums1[x] = result[x];
         }
