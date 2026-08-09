10e913b0bac0f4d88ddb0070c4b0179dd410a5b0
GALI LAZAR <thalapathyasuran@gmail.com>
2026-08-09 11:07:12 +0530

    Time: 1 ms (25.91%) | Memory: 54.5 MB (53.35%) - LeetSync

--- 520-detect-capital/detect-capital.js
+++ 520-detect-capital/detect-capital.js
@@ -4,8 +4,7 @@
  */
 var detectCapitalUse = function(word) {
     
-    return word === word.toUpperCase() ||
-           word === word.toLowerCase() ||
-           word[0] === word[0].toUpperCase() &&
-           word.slice(1) === word.slice(1).toLowerCase();
+    return  word === word.toUpperCase() || word === word.toLowerCase() || word[0] === word[0].toUpperCase() && word.slice(1) === word.slice(1).toLowerCase();
+            
+
 };
