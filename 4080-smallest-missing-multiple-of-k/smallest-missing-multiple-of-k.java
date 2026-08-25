7da592b35c408f1ad8bfb7757ba8b124571923fe
GALI LAZAR <thalapathyasuran@gmail.com>
2026-08-25 16:21:13 +0530

    Time: 1 ms (82.00%) | Memory: 44.9 MB (83.31%) - LeetSync

--- 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.java
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.java
@@ -1,22 +1,17 @@
 class Solution {
     public int missingMultiple(int[] nums, int k) {
 
-        int freq [] = new int [101];
+        int freq[] = new int[101];
 
-        for(int i = 0; i<nums.length; i++)
-        {
+        for (int i = 0; i < nums.length; i++) {
             freq[nums[i]]++;
         }
-        int multiple = k;
 
-        while(true)
-        {
-            if(multiple > 100 || freq[multiple] == 0)
-            {
-                return multiple;
+        for (int i = k; ; i += k) {
+
+            if (i > 100 || freq[i] == 0) {
+                return i;
             }
-            multiple += k;
         }
-        
     }
 }
