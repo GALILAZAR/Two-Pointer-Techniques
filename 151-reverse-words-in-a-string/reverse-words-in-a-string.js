f54ac0180c1ba511dcdd70630f3a335ed703e4c5
GALI LAZAR <thalapathyasuran@gmail.com>
2026-08-28 09:51:03 +0530

    Time: 1 ms (83.97%) | Memory: 55.3 MB (74.49%) - LeetSync

---
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+
+    let str = s.trim().split(/\s+/)
+    let arr = []
+    let index = 0;
+     
+     for(let i = str.length-1; i>=0; i--)
+     {
+        arr[index] = str[i];
+        index++;
+     }
+     return arr.join(" ").trim()
+    
+};
