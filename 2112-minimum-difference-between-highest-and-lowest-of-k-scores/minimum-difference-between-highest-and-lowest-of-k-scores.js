225dc3389fcaac8829bd91412d6ce4f164a281d6
GALI LAZAR <thalapathyasuran@gmail.com>
2026-08-28 09:06:36 +0530

    Time: 7 ms (82.00%) | Memory: 56.6 MB (66.00%) - LeetSync

---
+++ 2112-minimum-difference-between-highest-and-lowest-of-k-scores/minimum-difference-between-highest-and-lowest-of-k-scores.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var minimumDifference = function(nums, k) {
+    
+    if(k == 1)
+    {
+        return 0;
+    }
+    nums.sort((a,b) => a-b);
+    let Min = Infinity ;
+    for(let i = 0; i<=nums.length-k; i++)
+    {
+        let diff = nums[i+k-1]-nums[i];
+        Min = Math.min(Min,diff);
+    }
+    return Min;
+
+};
